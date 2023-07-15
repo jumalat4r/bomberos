d4d4c1a7dc8932d547eb228501f41a94860f3abc
Jazmin Rodriguez <131728571+jumalat4r@users.noreply.github.com>
2023-07-14 21:01:13 -0300

    Se inicia el proyecto

---
+++ apps/frontend/src/modules/authorization/components/banner.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <v-img
+      :width="300"
+      aspect-ratio="16/9"
+      cover
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    ></v-img>
+      
+    
+      </template>
+  
